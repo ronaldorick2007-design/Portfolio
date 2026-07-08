8ee4d10051c0e1a613f62d034a8ff9d3cd1e1610
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-07-08 08:15:22 +0530

    feat : method to initalize array

--- Grid.js
+++ Grid.js
@@ -13,6 +13,41 @@ class Grid{
         console.log(cols)
     }
 
+    setArray(array){
+        if(array[0].length != null){
+            this.cols = array[0].length;
+            this.is2D = true;
+            console.log(this.cols ,"from if");
+            this.gridSpace.style.gridTemplateColumns = `repeat(${this.cols},1fr)`;
+            for (let i = 0; i < array.length; i++) {    
+                let dots=[]        
+                for(let j=0; j<this.cols;j++){
+                        const dot = document.createElement("div");
+                        dot.className = "dot";
+                        this.gridSpace.append(dot);
+                        dot.hold = false;
+                        dot.textContent = `${array[i][j]}`;
+                        
+                        dots[j] = dot;
+                }
+                this.gridArray[i] = dots;
+            }
+        }
+        else{
+            console.log(this.cols,"from else");
+            this.gridSpace.style.gridTemplateColumns = `repeat(${array.length},1fr)`;
+            for (let i = 0; i < array.length; i++) {            
+                const dot = document.createElement("div");
+                dot.className = "dot";
+                this.gridSpace.append(dot);
+                dot.hold = false;
+                dot.textContent = `${array[i]}`;
+
+                this.gridArray[i] = dot
+            }
+        }
+    }
+
 
 }
 
