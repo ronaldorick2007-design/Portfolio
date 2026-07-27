f497c8cc0956fa9e2c08c76c89a2767f2ad9d2da
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-07-27 18:39:19 +0530

    feat: Initialize App component with core layout

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+export default function App(){
+  return(
+    <>
+    <Header />
+    <Body/>
+    <Footer/>
+    </>
+  );
+}
