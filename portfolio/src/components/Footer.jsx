70fb9df6df02f1a564e633c2a2baee02d51b10cc
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-07-28 21:14:36 +0530

    feat(footer): add initial Footer component

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer(){
+    return(
+        <div className="header">
+            <span id="logo">Ronald</span>
+            <div className="nav">
+                <a href="#">Home</a>
+                <a href="#">Home</a>
+                <a href="#">Home</a>
+            </div>
+        </div>
+    );
+}
