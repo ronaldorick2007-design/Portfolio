c1345da351f4697f62d4fa23a13c8a3a862a7513
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-07-29 19:10:01 +0530

    feat: Add linear search visualization component

---
+++ portfolio/src/components/LinearSearch.jsx
@@ -0,0 +1,55 @@
+import usePipeline from "../hooks/usePipeline";
+import linearSearch from "../algorithms/linearSearch";
+// import GridBuild from "./Grid";
+import LinkedList from "./LinkedList";
+import "../styles/Grid.css";
+import "../styles/Item.css";
+import GridBuild from "./Grid";
+
+export default function LinearSearch({arr}) {
+
+    const {
+        active,
+        start,
+        next,
+        stop,
+        match,
+        running,
+        pass,
+        cut
+    } = usePipeline(linearSearch);
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px",
+            }}
+        >
+            <div style={{ display: "flex",justifyContent:"center", gap: "10px" }}>
+                <button onClick={start}>
+                    {running ? "Pause" : "Play"}
+                </button>
+
+                <button onClick={next}>
+                    Step
+                </button>
+
+                <button onClick={stop}>
+                    Stop
+                </button>
+            </div>
+
+            <div className="container">
+                <GridBuild
+                    arr={arr}
+                    active={active}
+                    match={match}
+                    pass={pass}
+                    cut={cut}
+                />
+            </div>
+        </div>
+    );
+}
