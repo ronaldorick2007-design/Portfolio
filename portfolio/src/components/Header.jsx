d00ac92d29c967090873e2496bceb12d95d35369
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-07-29 19:09:56 +0530

    feat(header): Implement Header component

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import "../styles/Header.css"
+
+
+
+export default function Header(){
+    return(
+        <div className="header">
+            <span id="logo">Ronald</span>
+            <div className="nav">
+                <a href="#">Home</a>
+                <a href="#">Home</a>
+                <a href="#">Home</a>
+            </div>
+        </div>
+    );
+}
