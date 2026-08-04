1a98cfc188ff29d76fe0719fbbbb0fdeaa2a3cb3
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-04 08:59:36 +0530

    feat(routing): Implement client-side routing

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,14 +1,19 @@
 import "./App.css"
-import Body from "./components/Body";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// import Body from "./components/Body";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Home from "./pages/Home";
+import Codes from "./pages/Codes";
 
 export default function App(){
   return(
-    <>
-    <Header />
-    <Body/>
-    <Footer/>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/codes" element = {<Codes/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
