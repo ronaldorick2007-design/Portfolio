d5f1b6cceff6c723b63d67e3b0d00dd10a3999f3
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-14 19:52:11 +0530

    feat(arrays): Add Bubble Sort visualization and code display

---
+++ portfolio/src/codeVisuals/Arrays/Code_bubble.jsx
@@ -0,0 +1,110 @@
+import "../../styles/Code.css";
+import "../../styles/Grid.css";
+import Window from "../../components/Window";
+import useArray from "../../hooks/useArray";
+import GridBuild from "../../components/Grid";
+import bubbleSort from "../../algorithms/Arrays/bubbleSort";
+
+
+// import "../styles/Code.css"
+// import Window from "../components/Window";
+// import BubbleSort from "../visuals/BubbleSort";
+
+  
+function BubbleSort() {
+
+    const {
+        arr,
+
+        active,
+        match,
+        swap,
+        pass,
+        hold,
+        log,
+        
+        running,
+        start,
+        stop,
+        next
+    } = useArray(bubbleSort,[5, 3, 8, 4, 2]);
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px",
+            }}
+        >
+            <div style={{ display: "flex",justifyContent:"center", gap: "10px" }}>
+                <button onClick={start}>
+                    {running ? "Pause" : "Play"}
+                </button>
+
+                <button onClick={next}>
+                    Step
+                </button>
+
+                <button onClick={stop}>
+                    Stop
+                </button>
+            </div>
+
+            <div className="container">
+                <GridBuild
+                    arr={arr}
+                    active={active}
+                    match={match}
+                    swap={swap}
+                    pass={pass}
+                    hold={hold}
+                />
+            </div>
+            <div>{log}</div>
+        </div>
+    );
+}
+ 
+export default function Code_bubble(){
+  const code = (
+    <pre>
+        <code>{`function bubbleSort(arr) {
+    let N = arr.length;
+    for (let i = 0; i < N - 1; i++) {
+        for (let j = 0; j < N - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+const A = [1, 4, 2, 5, 3];
+bubbleSort(A);`}</code>
+    </pre>
+);
+
+    return(
+        <div className="content Hover">
+    <div className="card-title alignText">
+      <h2>Bubble Sort</h2>
+    </div>
+    <div className="content-inner">
+      <div className="card-about alignText">
+        {/* <LinearSearch arr={[1,3,5,2,3]}/> */}
+        <BubbleSort />
+        {/* <TreeBuild arr={[0,1,2,3,4,5,6]} size={30}/> */}
+      </div>
+      <div className="sidebar alignText">
+        <h3>Sidebar</h3>
+        <p>This is the code content inside code box.</p>
+        <Window code={code}/>
+      </div>
+    </div>
+  </div>
+    );
+}
