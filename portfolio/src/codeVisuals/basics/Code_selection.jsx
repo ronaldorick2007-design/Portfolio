602d2626d1956a83ca7119862fa0707586298899
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-17 09:00:38 +0530

    feat(codeVisuals): Add selection sort algorithm visualization and code snippet

---
+++ portfolio/src/codeVisuals/basics/Code_selection.jsx
@@ -0,0 +1,100 @@
+import "../../styles/Code.css";
+import Window from "../../components/Window";
+import useArray from "../../hooks/useArray";
+import GridBuild from "../../components/Grid";
+import selection from "../../algorithms/basics/selection";
+
+function Selection() {
+
+    const {
+        arr,
+
+        active,
+        match,
+        swap,
+        pass,
+        hold,
+        log,
+        
+        running,
+        start,
+        stop,
+        next
+    } = useArray(selection,[1, 2, 3, 4, 5]);
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px",
+            }}
+        >
+            <div style={{ display: "flex",justifyContent:"center", gap: "10px" }}>
+                <button onClick={start}>
+                    {running ? "Pause" : "Play"}
+                </button>
+
+                <button onClick={next}>
+                    Step
+                </button>
+
+                <button onClick={stop}>
+                    Stop
+                </button>
+            </div>
+
+            <div className="container">
+                <GridBuild
+                    arr={arr}
+                    active={active}
+                    match={match}
+                    swap={swap}
+                    pass={pass}
+                    hold={hold}
+                />
+            </div>
+
+            <div>{log}</div>
+        </div>
+    );
+}
+
+
+export default function Code_selection(){
+  const code = (
+    <pre>
+        <code>{`const arr = [1, 2, 3, 4, 5];
+const n = arr.length; 
+
+for(let i = 0; i < n; i++){
+    console.log(arr[i]);
+    if(i == 2){
+    console.log("condition met!")
+    }
+    else{
+        continue;
+    }
+}`
+    }</code>
+    </pre>
+);
+
+    return(
+        <div className="content Hover">
+    <div className="card-title alignText">
+      <h2>If-else selection</h2>
+    </div>
+    <div className="content-inner">
+      <div className="card-about alignText">
+        <Selection />
+      </div>
+      <div className="sidebar alignText">
+        <h3>Code Snippet</h3>
+        <p>This is the code to iterate an array using for loop</p>
+        <Window code={code}/>
+      </div>
+    </div>
+  </div>
+    );
+}
