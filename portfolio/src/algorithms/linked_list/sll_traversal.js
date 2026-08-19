bb131b5331166813a65f1be09fd53d0973d889f5
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-19 19:59:21 +0530

    feat(sll): Add singly linked list operations generator

---
+++ portfolio/src/algorithms/linked_list/sll_traversal.js
@@ -0,0 +1,28 @@
+import { SinglyLinkedList } from "../../data_structures/SinglyLinkedList"
+
+export default function* sll_traversal(){
+    const sll = new SinglyLinkedList();
+    yield [
+        {action:"set", index : sll},
+        {action:"log", index:["Initialized singly linked list"]}
+    ];
+
+    for(let i=0;i<5;i++){
+        sll.add(i);
+        yield [
+        {action:"rearr", index : []},
+        {action:"log", index:[`Adding new node ${i}`]}
+    ];
+    }
+
+    for(let i=0;i<5;i++){
+        sll.pop();
+        yield [
+        {action:"rearr", index : []},
+        {action:"log", index:[`Popped node ${i}`]}
+    ];
+    }
+
+    
+
+}
