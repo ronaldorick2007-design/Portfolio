958c71736c2ec1dfd2934d889ba5b555c1ab192f
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-20 09:21:36 +0530

    feat(sll_traversal): increase initial node count; add commented traversal block

--- portfolio/src/algorithms/linked_list/sll_traversal.js
+++ portfolio/src/algorithms/linked_list/sll_traversal.js
@@ -7,13 +7,19 @@ export default function* sll_traversal(){
         {action:"log", index:["Initialized singly linked list"]}
     ];
 
-    for(let i=0;i<5;i++){
+    for(let i=0;i<10;i++){
         sll.add(i);
         yield [
         {action:"rearr", index : []},
         {action:"log", index:[`Adding new node ${i}`]}
     ];
     }
+
+    // let curr = sll.head;
+    // while(curr != null){
+    //     yield [{action:"indicate", index : {[curr] : "active"}},];
+    //     curr = curr.next;
+    // }
 
     for(let i=0;i<5;i++){
         sll.pop();
