76b5f0bec5ac9412a541ad563deed2a5d8d06013
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-20 09:21:40 +0530

    feat: Enhance stack push/pop visualization and logs

--- portfolio/src/algorithms/stack/stack_push.js
+++ portfolio/src/algorithms/stack/stack_push.js
@@ -1,17 +1,19 @@
 import { Stack } from "../../data_structures/Stack";
-
+ 
 export default function* stack_push(){
     const sll = new Stack()
     yield [
         {action:"set", index : sll},
-        {action:"log", index:["Initialized singly linked list"]}
+        {action:"log", index:["Initialized Stack"]}
     ];
 
     for(let i=0;i<5;i++){
         sll.push(i);
+        
         yield [
         {action:"rearr", index : []},
-        {action:"log", index:[`Adding new node ${i}`]}
+        {action:"indicate", index : {[sll.head] : "active"}},
+        {action:"log", index:[`pushing new value ${i}`]}
     ];
     }
 
@@ -19,7 +21,8 @@ export default function* stack_push(){
         sll.pop();
         yield [
         {action:"rearr", index : []},
-        {action:"log", index:[`Popped node ${i}`]}
+        {action:"indicate", index : {[sll.head] : "active"}},
+        {action:"log", index:[`Popped value ${i}`]}
     ];
     }
 
