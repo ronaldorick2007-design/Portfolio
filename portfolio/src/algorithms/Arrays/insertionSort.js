54d7b6b90c507f51a7205db9a05909ad68be541d
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-23 16:50:52 +0530

    feat: Make insertionSort generator self-contained for visualization

--- portfolio/src/algorithms/Arrays/insertionSort.js
+++ portfolio/src/algorithms/Arrays/insertionSort.js
@@ -1,10 +1,16 @@
-export default function* insertionSort(arr){
-    let N = arr.length;
+export default function* insertionSort(){
+    
+    const arr = [5, 3, 8, 4, 2]
+    const n = arr.length;
+ 
+    yield [
+            { action: "set",type:"A", index: arr, name: "arr" },
+        ];
 
-    for (let i = 1; i < N; i++) {
+    for (let i = 1; i < n; i++) {
         let key = arr[i];
         yield[
-            { action : "hold", index : [i]},
+            { action : "hold", index : [i], name: "arr"},
             { action : "log", index : [`Current Index ${i}`]}
         ]
         let j = i - 1;
@@ -12,15 +18,15 @@ export default function* insertionSort(arr){
         while (j >= 0 && arr[j] > key) {
             yield[{ action : "log", index : ["previous value is greater."]}]
             yield[
-                { action : "swap", index : {[j]:j+1, [j+1]:j}},
+                { action : "swap", index : {[j]:j+1, [j+1]:j}, name: "arr"},
                 { action : "log", index : [`Swap with previous : ${j}`]}
             ]
             arr[j + 1] = arr[j];
             j--;
             arr[j + 1] = key;
             yield [
-                { action : "rearrange", index : []},
-                { action : "hold", index : [j+1]}
+                { action : "rearrange", index : [], name: "arr"},
+                { action : "hold", index : [j+1], name: "arr"}
             ]
         }
         
