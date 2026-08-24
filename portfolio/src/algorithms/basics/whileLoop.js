8a7af2914798f8db70eae44b89e2f0b09a93ca0a
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-24 09:00:57 +0530

    feat(whileLoop): Internalize array and enhance state tracking for visualization

--- portfolio/src/algorithms/basics/whileLoop.js
+++ portfolio/src/algorithms/basics/whileLoop.js
@@ -1,10 +1,14 @@
-export default function* whileLoop(arr){
+export default function* whileLoop(){
+    const arr = [1,2,3,4,5];
+    yield [
+            { action: "set",type:"A", index: arr, name: "arr" },
+        ];
     const n = arr.length;
     let i = 0;
     
     while(i < n){
         yield [
-            { action: "active", index: [i] },
+            { action: "active", index: [i], name: "arr" },
             { action: "log", index: [`Current index : ${i} < ${n}`] },
         ];
         i++;
