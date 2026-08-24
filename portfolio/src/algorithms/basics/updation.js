272437d951ea148a2954c5e3e94b099bee3383e7
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-24 09:00:48 +0530

    refactor(updation): Encapsulate array initialization and specify data structure in actions

--- portfolio/src/algorithms/basics/updation.js
+++ portfolio/src/algorithms/basics/updation.js
@@ -1,25 +1,29 @@
-export default function* updation(arr){
+export default function* updation(){
+    const arr = [1,2,3,4,5];
+    yield [
+            { action: "set",type:"A", index: arr, name: "arr" },
+        ];
     let n = arr.length;
     for(let i = 0; i < n; i++){
         yield [
-            { action : "active", index : [i]},
+            { action : "active", index : [i], name: "arr" },
             { action : "log", index : [`Current index ${i}`]}
         ];
 
         if(i == 2){
             yield [
-                { action : "match", index : [i]},
-                { action : "active", index : []},
+                { action : "match", index : [i], name: "arr"},
+                { action : "active", index : [], name: "arr"},
                 { action : "log", index : [`Match Found`]}
             ]
             arr[i] = 7
             yield [
-                { action : "match", index : []},
-                { action : "change", index : [i]},
+                { action : "match", index : [], name: "arr"},
+                { action : "change", index : [i], name: "arr"},
                 { action : "log", index : [`Updating with new value`]}
             ]
 
-            yield { action : "change", index : []}
+            yield { action : "change", index : [], name: "arr"}
         }
     }
 
