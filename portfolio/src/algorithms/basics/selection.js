8ad7d65100ad79c6ec9ebb8eacd2e79f2fc9f642
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-24 09:00:43 +0530

    refactor(selection): Initialize internal array and clarify visualization actions

--- portfolio/src/algorithms/basics/selection.js
+++ portfolio/src/algorithms/basics/selection.js
@@ -1,15 +1,19 @@
-export default function* selection(arr){
+export default function* selection(){
+    const arr = [1, 2, 3, 4, 5]
+    yield [
+            { action: "set",type:"A", index: arr, name: "arr" },
+        ];
     const n = arr.length;
         
     for(let i=0;i<n;i++){
         yield [
-            { action: "active", index: [i] },
+            { action: "active", index: [i], name: "arr" },
             { action: "log", index: [`Current index : ${i}`] },
         ];
 
         if(i == 2){
             yield [
-            { action: "match", index: [i] },
+            { action: "match", index: [i], name: "arr" },
             { action: "log", index: [`Condition met!`] },
         ];
         }
