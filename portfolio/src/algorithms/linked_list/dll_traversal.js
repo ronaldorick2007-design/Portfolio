b6af0d6d8d877c1dec7d669a5fadd645847ce2eb
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-24 09:01:05 +0530

    feat(algorithms): Add list type and name to DLL traversal visualization actions

--- portfolio/src/algorithms/linked_list/dll_traversal.js
+++ portfolio/src/algorithms/linked_list/dll_traversal.js
@@ -3,14 +3,14 @@ import { DoublyLinkedList } from "../../data_structures/DoublyLinkedList";
 export default function* dll_traversal(){
     const dll = new DoublyLinkedList();
     yield [
-        {action:"set", index : dll},
+        {action:"set", index : dll, type:'L', name:"dll"},
         {action:"log", index:["Initialized doubly linked list"]}
     ];
 
     for(let i=0;i<5;i++){
         dll.add(i);
         yield [
-        {action:"rearr", index : []},
+        {action:"rearr", index : [], name:"dll"},
         {action:"log", index:[`Adding new node ${i}`]}
     ];
     }
@@ -18,7 +18,7 @@ export default function* dll_traversal(){
     for(let i=0;i<5;i++){
         dll.pop();
         yield [
-        {action:"rearr", index : []},
+        {action:"rearr", index : [], name:"dll"},
         {action:"log", index:[`Popped node ${i}`]}
     ];
     }   
