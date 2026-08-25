75412aa260253cb1fbf3427877b6043eb3343a79
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-25 15:46:21 +0530

    feat: Add visualization metadata to SLL traversal actions; reduce demo length

--- portfolio/src/algorithms/linked_list/sll_traversal.js
+++ portfolio/src/algorithms/linked_list/sll_traversal.js
@@ -3,14 +3,14 @@ import { SinglyLinkedList } from "../../data_structures/SinglyLinkedList"
 export default function* sll_traversal(){
     const sll = new SinglyLinkedList();
     yield [
-        {action:"set", index : sll},
+        {action:"set", index : sll, type:'L', name:"sll"},
         {action:"log", index:["Initialized singly linked list"]}
     ];
 
-    for(let i=0;i<10;i++){
+    for(let i=0;i<5;i++){
         sll.add(i);
         yield [
-        {action:"rearr", index : []},
+        {action:"rearr", index : [], name:"sll"},
         {action:"log", index:[`Adding new node ${i}`]}
     ];
     }
@@ -24,7 +24,7 @@ export default function* sll_traversal(){
     for(let i=0;i<5;i++){
         sll.pop();
         yield [
-        {action:"rearr", index : []},
+        {action:"rearr", index : [], name:"sll"},
         {action:"log", index:[`Popped node ${i}`]}
     ];
     }
