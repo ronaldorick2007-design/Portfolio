e428df29da31757c41c9595d3ea4876a977f41e7
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-29 18:19:06 +0530

    feat: Implement algorithm visualization steps using generator

--- portfolio/src/algorithms/Miscellanous/specific.js
+++ portfolio/src/algorithms/Miscellanous/specific.js
@@ -1,7 +1,37 @@
-export default function specific(n){
-    console.log("Entering ",n);
-    if(n==1){console.log("Exiting ",n); return 1;}
-    let temp = specific(n-1);
-    console.log("Exiting ",n);
-    return n * temp;
+export default function* specific(){
+    const arr = [1,2,3,4,5]
+
+    yield [
+        {action:"set",type:'A',name:"arr",index:arr,scope:"main"}
+    ]
+
+    yield [
+        {action:"indicate",index:[[arr[0],arr[4]],"hold"]}
+    ]
+
+    yield [
+        {action:"swap",index:{[0]:4,[4]:0},name:"arr",scope:"main"}
+    ]
+    
+    // [arr[0], arr[1]] = [arr[1], arr[0]];
+    let temp = arr[0];
+    arr[0] = arr[4];
+    arr[4] = temp;
+
+    console.log(arr)
+    yield [{ action: "rearrange", index: [...arr], name: "arr",scope:"main" },
+        {action:"indicate",index:[arr[2],"active"]}
+    ]
+
+    yield [
+        {action:"indicate",index:[[...arr.slice(2)],"hold"]}
+    ]
+
+    yield [
+        {action:"indicate",index:[arr[1],"active"]}
+    ]
+
+    yield [
+        {action:"indicate",index:[arr[1],"match"]}
+    ]
 }
