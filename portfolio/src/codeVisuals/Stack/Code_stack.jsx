276e6e950a6b8b246658cd74f104ca236c782493
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-31 19:10:46 +0530

    feat : implement stack ds

---
+++ portfolio/src/codeVisuals/Stack/Code_stack.jsx
@@ -0,0 +1,89 @@
+import "../../styles/Code.css";
+import "../../styles/Grid.css";
+import Window from "../../components/Window";
+import useArray from "../../hooks/useArray";
+import GridBuild from "../../components/Grid";
+import selectionSort from "../../algorithms/Arrays/selectionSort";
+
+import useNode from "../../hooks/useNode";
+import LinkedList from "../../components/LinkedList";
+import Stack from "../../components/Stack";
+import stack_basic from "../../algorithms/stack/stack_basic";
+import sll_traversal from "../../algorithms/linked_list/sll_traversal";
+
+function StackPush() {
+
+    const {
+        arr,
+        log,
+        indicate,
+        
+        running,
+        start,
+        stop,
+        next
+    } = useNode(stack_basic);
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px",
+            }}
+        >
+            <div style={{ display: "flex",justifyContent:"center", gap: "10px" }}>
+                <button onClick={start}>
+                    {running ? "Pause" : "Play"}
+                </button>
+
+                <button onClick={next}>
+                    Step
+                </button>
+
+                <button onClick={stop}>
+                    Stop
+                </button>
+            </div>
+
+                <LinkedList arr={arr} indicate={indicate}/>
+
+            <div>{log}</div>
+        </div>
+    );
+}
+ 
+export default function Code_stack(){
+  const code = (
+    <pre>
+        <code>{
+`const sll = new SinglyLinkedList();
+
+for(let i = 0; i < 5; i++){
+            sll.add(i);
+}
+
+for(let i = 0; i < 5; i++){
+            sll.pop();
+}`}</code>
+    </pre>
+);
+
+    return(
+        <div className="content Hover">
+    <div className="card-title alignText">
+      <h2>Stack</h2>
+    </div>
+    <div className="content-inner">
+      <div className="card-about alignText">
+        <StackPush />
+      </div>
+      <div className="sidebar alignText">
+        <h3>Sidebar</h3>
+        <p>This is the code content inside code box.</p>
+        <Window code={code}/>
+      </div>
+    </div>
+  </div>
+    );
+}
