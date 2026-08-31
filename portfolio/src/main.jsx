20a09dd122ebab4854b61334888a7588f510a539
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-08-31 10:50:46 +0530

    feat: Integrate ThemeProvider for application theming

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import "./styles/index.css"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+// import "./index.css";
+import "./styles/index.css"
+import App from "./App.jsx";
+// import { ThemeProvider } from "./ThemeProvider.jsx";
+import { ThemeProvider } from "./components/ThemeProvider.jsx";
+
+createRoot(document.getElementById("root")).render(
   // <StrictMode>
-    <App />
-  // </StrictMode>,
-)
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  // </StrictMode>
+);
