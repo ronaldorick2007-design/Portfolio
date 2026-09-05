56335af9b837f2b65c64e96adb5fde27fddb0935
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-09-05 15:20:52 +0530

    feat(specific): Set up Node data structure initialization

--- portfolio/src/algorithms/Miscellanous/specific.js
+++ portfolio/src/algorithms/Miscellanous/specific.js
@@ -1,63 +1,72 @@
+import { Node } from "../../data_structures/Node";
+
 export default function* specific()
 //-------------------------------------------------------------------
 {
-    const arr = [1,2,3,4,5];
+    const arr = new Node(1);
+    console.log(arr)
     yield [
-            { action: "set",type:"A", index: arr, name: "arr" },
+            { action: "set",type:"N", index: arr, name: "arr" },
         ];
-    const target = 1;
- 
-    let left = 0;
-    let right = arr.length - 1;
-    let status ="";
-    let clear;
 
-    console.log(left,right)
-    yield  [{ action: "indicate", index: [[left, right],"hold"], name: "arr" },
-            { action: "log", index: [`left : ${left}\nright : ${right}`]}]
+    arr.value = 2;
+    console.log(arr)
+    yield [
+            { action: "rearrange",type:"N", index: arr, name: "arr" },
+        ];
+    // const target = 1;
+ 
+    // let left = 0;
+    // let right = arr.length - 1;
+    // let status ="";
+    // let clear;
+
+    // console.log(left,right)
+    // yield  [{ action: "indicate", index: [[left, right],"hold"], name: "arr" },
+    //         { action: "log", index: [`left : ${left}\nright : ${right}`]}]
             
 
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        // Highlight current window bounds (left and right) and mid
-        yield [
-            // { action: "hold", index: [] },
-            {action:"indicate",index:[mid,"active"],name:"arr"},
-            { action: "log", index: [`Middle index : ${mid}`]}
-            // { action: "hold", index: [left, right] }
-        ];
+    // while (left <= right) {
+    //     const mid = Math.floor((left + right) / 2);
+    //     // Highlight current window bounds (left and right) and mid
+    //     yield [
+    //         // { action: "hold", index: [] },
+    //         {action:"indicate",index:[mid,"active"],name:"arr"},
+    //         { action: "log", index: [`Middle index : ${mid}`]}
+    //         // { action: "hold", index: [left, right] }
+    //     ];
 
-        // Found target
-        if (arr[mid] === target) {
-            yield [
-                {action:"indicate",index:[mid,"match"],name:"arr"}, 
-                { action: "log", index: [`Target found!`]}               
-                // { action: "cut", index: mid }
-            ];
-            return;
-        }
+    //     // Found target
+    //     if (arr[mid] === target) {
+    //         yield [
+    //             {action:"indicate",index:[mid,"match"],name:"arr"}, 
+    //             { action: "log", index: [`Target found!`]}               
+    //             // { action: "cut", index: mid }
+    //         ];
+    //         return;
+    //     }
 
-        // Adjust boundaries and eliminate sub-array range
-        if (arr[mid] < target) {
-            // Cut everything from left up to mid
-            clear = Array.from({ length: mid - left + 1 }, (_, i) => left + i);
-            left = mid + 1;
-            status = `Shift left to ${left}`;
+    //     // Adjust boundaries and eliminate sub-array range
+    //     if (arr[mid] < target) {
+    //         // Cut everything from left up to mid
+    //         clear = Array.from({ length: mid - left + 1 }, (_, i) => left + i);
+    //         left = mid + 1;
+    //         status = `Shift left to ${left}`;
 
-        } else {
-            // Cut everything from mid up to right
-            clear = Array.from({ length: right - mid + 1 }, (_, i) => mid + i);
-            right = mid - 1;
-            status = `Shift right to ${right}`
+    //     } else {
+    //         // Cut everything from mid up to right
+    //         clear = Array.from({ length: right - mid + 1 }, (_, i) => mid + i);
+    //         right = mid - 1;
+    //         status = `Shift right to ${right}`
 
-        }
+    //     }
 
-        yield [
-                { action: "indicate", index: [[left, right],"hold"], name: "arr" },
-                { action: "indicate", index: [clear,"pass"], name: "arr" },
-                { action: "log", index: [status]}
-            ];
-    }
+    //     yield [
+    //             { action: "indicate", index: [[left, right],"hold"], name: "arr" },
+    //             { action: "indicate", index: [clear,"pass"], name: "arr" },
+    //             { action: "log", index: [status]}
+    //         ];
+    // }
 }
 //----------------------------------------------------------------
 // {
