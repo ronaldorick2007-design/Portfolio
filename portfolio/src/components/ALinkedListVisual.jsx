e213b9b2c8ed7872b61cc60b0ee02155e4d35b62
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-09-06 18:33:49 +0530

    refactor: LinkedListVisual: Migrate to useLinkedList hook

--- portfolio/src/components/ALinkedListVisual.jsx
+++ portfolio/src/components/ALinkedListVisual.jsx
@@ -1,11 +1,12 @@
 import { forwardRef, useImperativeHandle } from "react";
 
 import useNode from "../hooks/useNode_renders";
+import useLinkedList from "../hooks/useLinkedList_renders";
 import LinkedList from "./LinkedList";
 
 const LinkedListVisual = forwardRef(function ArrayVisual(_, ref) {
 
-    const { name,arr,indicate, apply} = useNode();
+    const { name,arr,indicate, apply} = useLinkedList();
     useImperativeHandle(ref, () => ({
         apply
     }), [apply]);
