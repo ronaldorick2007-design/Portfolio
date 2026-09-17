d47e8d817c7182eccd57891e84437f0799a99c25
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-09-17 08:04:06 +0530

    chore: Remove debug log from preorder tree traversal

--- portfolio/src/algorithms/trees/preorder.js
+++ portfolio/src/algorithms/trees/preorder.js
@@ -72,7 +72,6 @@ function* sll_traversal(head){
     let curr = head;
 
     while(curr !== null){
-        console.log(curr)
         yield [
         {action:"indicate",index:[curr,"active"]},
         ]
