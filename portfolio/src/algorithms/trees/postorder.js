d22d7433db600b4b4661501957e1d3d445a9fc04
ronaldorick2007-design <ronaldorick2007@gmail.com>
2026-09-17 08:04:01 +0530

    refactor: Remove debug log from sll_traversal

--- portfolio/src/algorithms/trees/postorder.js
+++ portfolio/src/algorithms/trees/postorder.js
@@ -73,7 +73,6 @@ function* sll_traversal(head){
     let curr = head;
 
     while(curr !== null){
-        console.log(curr)
         yield [
         {action:"indicate",index:[curr,"active"]},
         ]
